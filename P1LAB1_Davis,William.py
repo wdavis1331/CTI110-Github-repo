60a25b34f7dacf28e09a0d6d59de8c3d44ba85a5
wdavis1331 <wdavis1331@gmail.com>
2025-09-18 11:43:05 -0400

    lab

--- P1LAB1_Davis,William.py
+++ P1LAB1_Davis,William.py
@@ -11,4 +11,4 @@ first_name = input("Please enter your first name: ")
 last_name = input("Please enter your last name: ")
 
 # Display the welcome message with the user's input
-print("Welcome to this course,", first_name, last_name + "!")
+print("Welcome to CTI-110,", first_name, last_name + "!")
